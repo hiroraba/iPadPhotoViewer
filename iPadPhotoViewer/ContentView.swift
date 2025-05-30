217c65abf1fd3be5407d3e8af2c90afddd66170e
hiroraba <xornelius0313@gmail.com>
2025-05-30 23:54:08 +0900

    Initial Commit

---
+++ iPadPhotoViewer/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  iPadPhotoViewer
+//  
+//  Created by matsuohiroki on 2025/05/30.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iPadPhotoViewer/iPadPhotoViewerApp.swift
@@ -0,0 +1,18 @@
+//
+//  iPadPhotoViewerApp.swift
+//  iPadPhotoViewer
+//  
+//  Created by matsuohiroki on 2025/05/30.
+//  
+//
+
+import SwiftUI
+
+@main
+struct iPadPhotoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
